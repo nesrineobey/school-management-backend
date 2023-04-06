a4eb1b5d2d358d1fb4590f7756d7c8f4318eefa9
nesrineobey <nesrineobey@gmail.com>
2023-04-06 22:37:42 +0200

    first commit

---
+++ src/main/java/com/sesame/GestionEcolePrimaire/GestionEcolePrimaireApplication.java
@@ -0,0 +1,13 @@
+package com.sesame.GestionEcolePrimaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEcolePrimaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEcolePrimaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesame/GestionEcolePrimaire/GestionEcolePrimaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesame.GestionEcolePrimaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEcolePrimaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
